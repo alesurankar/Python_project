d6ccfa3ad7c9c9d45df97cfbcad51a4c1abfad2f
ales2026 <ale.urankar@gmail.com>
2025-08-28 09:57:18 +0200

    guess_number 2

--- guess_number/main.py
+++ guess_number/main.py
@@ -4,7 +4,7 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = input('Guess a number between 1 and {x}')
+        guess = int(input(f'Guess a number between 1 and {x}: '))
         print(guess)
 
 guess(10)
