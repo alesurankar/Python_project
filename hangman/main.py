8d5c05d8fc565beac9fe90678e6a3528d4180774
ales2026 <ale.urankar@gmail.com>
2025-08-29 21:04:04 +0200

    input support

--- hangman/main.py
+++ hangman/main.py
@@ -1,3 +1,19 @@
+import random
 from words import words
+import string
 
-print(words)
+def get_valid_word(words):
+    word = random.chouce(words) #randomly chooses something from the list
+    while '_' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.asci_uppercase)
+    used_letters = set()  #what the user has guessed
+
+user_input = input('Type something:')
+print(user_input)
