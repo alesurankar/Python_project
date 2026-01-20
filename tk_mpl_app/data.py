921733a22fa8d41cbf0cdc1ea7acee3064753884
ales2026 <ale.urankar@gmail.com>
2026-01-20 14:24:54 +0100

    more custom data entry

--- tk_mpl_app/data.py
+++ tk_mpl_app/data.py
@@ -1,12 +1,27 @@
+############
+# my data
+############
+my_title = 'Sunny Days per Month'
+my_label = 'Sunny Days'
+my_xLabel = 'Month'
+my_yLabel = 'Sunny Days'
+
+my_x = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+my_y = [8,10,7,14,20,18,25,19,18,14,12,7]
+
+my_avg = False
+my_avg_label = 'Average Sunny Days'
+
+
 class Data:
-    title = 'Sunny Days per Month'
-    label = 'Sunny Days'
-    xLabel = 'Month'
-    yLabel = 'Sunny Days'
+    title = my_title
+    label = my_label
+    xLabel = my_xLabel
+    yLabel = my_yLabel
 
-    x = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-    y = [8,10,7,14,20,18,25,19,18,14,12,7]
+    x = my_x
+    y = my_y
 
-    avg = False
-    avg_label = 'Average Sunny Days'
+    avg = my_avg
+    avg_label = my_avg_label
     avg_value = sum(y) / len(y)
