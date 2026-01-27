c54a7524ec47ba49d067422a4fb48b69a39ba383
ales2026 <ale.urankar@gmail.com>
2026-01-27 08:38:40 +0100

    fixing issues in navigation.py

--- tk_mpl_app/gui/navigation.py
+++ tk_mpl_app/gui/navigation.py
@@ -1,7 +1,7 @@
-import tkinter as ttk
+from tkinter import ttk
 
 def make_navigation_buttons(top_frame, show_next, show_prev):
+    prev_button = ttk.Button(top_frame, text="Previous", command=show_prev)
+    prev_button.pack(side="left", padx=5)
     next_button = ttk.Button(top_frame, text="Next", command=show_next)
     next_button.pack(side="right", padx=5)
-    prev_button = ttk.Button(top_frame, text="Previous", command=show_prev)
-    prev_button.pack(side="right", padx=5)
