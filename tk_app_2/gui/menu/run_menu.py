c875ee4bd5b31d6c3b4f9d4ff1da66a17b0716e4
ales2026 <ale.urankar@gmail.com>
2026-02-11 16:03:57 +0100

    adding run_menu and terminal_menu

--- tk_app_2/gui/menu/run_menu.py
+++ tk_app_2/gui/menu/run_menu.py
@@ -3,6 +3,33 @@ import tkinter as tk
 
 def create_run_menu(root, menubar):
     run_menu = tk.Menu(menubar, tearoff=0)
+    
+    run_menu.add_command(label="Start Debugging")
+    run_menu.add_command(label="Run Without Debugging")
+    run_menu.add_command(label="Stop Debugging", state="disabled")
+    run_menu.add_command(label="Restart Debugging", state="disabled")
+    run_menu.add_separator()
+
+    run_menu.add_command(label="Open Configurations", state="disabled")
+    run_menu.add_command(label="Add Configurations...")
+    run_menu.add_separator()
+    
+    run_menu.add_command(label="Step Over", state="disabled")
+    run_menu.add_command(label="Step Into", state="disabled")
+    run_menu.add_command(label="Step Out", state="disabled")
+    run_menu.add_command(label="continue", state="disabled")
+    run_menu.add_separator()
+
+    run_menu.add_command(label="Toggle Breakpoint")
+    run_menu.add_command(label="New Breakpoint")
+    run_menu.add_separator()
+
+    run_menu.add_command(label="Enable All Breakpoints")
+    run_menu.add_command(label="Disable All Breakpoints")
+    run_menu.add_command(label="Remove All Breakpoints")
+    run_menu.add_separator()
+    
+    run_menu.add_command(label="Install Additional Debuggers...")
 
     menubar.add_cascade(label="Run", menu=run_menu)
     return run_menu
--- tk_app_2/gui/menu/terminal_menu.py
+++ tk_app_2/gui/menu/terminal_menu.py
@@ -4,5 +4,24 @@ import tkinter as tk
 def create_terminal_menu(root, menubar):
     terminal_menu = tk.Menu(menubar, tearoff=0)
 
+    terminal_menu.add_command(label="New Terminal")
+    terminal_menu.add_command(label="Split Terminal")
+    terminal_menu.add_command(label="New Terminal Window")
+    terminal_menu.add_separator()
+    
+    terminal_menu.add_command(label="Run Task...")
+    terminal_menu.add_command(label="Run Build Task...")
+    terminal_menu.add_command(label="Run Active File...")
+    terminal_menu.add_command(label="Run Selected Text...")
+    terminal_menu.add_separator()
+    
+    terminal_menu.add_command(label="Show Running Tasks...")
+    terminal_menu.add_command(label="Restart Running Task...")
+    terminal_menu.add_command(label="Terminate Task...")
+    terminal_menu.add_separator()
+    
+    terminal_menu.add_command(label="Configure Tasks...")
+    terminal_menu.add_command(label="Configure Default Build Task...")
+
     menubar.add_cascade(label="Terminal", menu=terminal_menu)
     return terminal_menu
